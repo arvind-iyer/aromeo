07db6370c947e7351b0a74d8d4323e489f94b1a3
Arvind Iyer <arvindi.me@gmail.com>
2016-11-09 16:27:50 +0800

    Initial commit. Server and Client Prototypes

---
+++ aromeoclient/app.js
@@ -0,0 +1,26 @@
+var CronJob = require('cron').CronJob;
+var request = require('request');
+
+var jobs = [];
+new CronJob('30 * * * * *', function() {
+
+    request('http://aromeo123123.herokuapp.com/tasks', function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            console.log(body);
+            jobs = JSON.parse(body);
+
+        }
+        if(jobs.length > 0) {
+            jobs.forEach(function(j) {
+                console.log(j.title + "\t" + j.crontime);
+            });
+        }
+
+    });
+},
+            function(){},
+            true,
+            'Asia/Hong_Kong'
+           );
+
+
